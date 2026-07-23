3fe8aef2ff396fc7b86a230069077c75dd74d1fb
JPostigo48 <jpostigo@unsa.edu.pe>
2026-07-23 09:30:16 -0500

    chore(gestion-rutas): actualizar encabezados de repositorios

--- apps/backend/src/modules/gestion-rutas/repositorio/prisma/prisma-horario-ruta.repository.ts
+++ apps/backend/src/modules/gestion-rutas/repositorio/prisma/prisma-horario-ruta.repository.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../../../../shared/infrastructure/prisma/prisma.service';
+import { PrismaService } from '../../../../shared/repositorio/prisma/prisma.service';
 import { HorarioRuta } from '../../dominio/entities/horario-ruta.entity';
 import {
   ActualizarHorarioRutaDatos,
